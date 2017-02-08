4d80dfd481bc60d72b365ef88fd9e52a703289a4
Caio Brighenti <Caio Brighenti>
2017-02-08 02:07:53 -0500

    Updates to Histogram.java: User now inputs each value for histogram, and histogram sorts itself

--- Histogram/Histogram.java
+++ Histogram/Histogram.java
@@ -5,23 +5,81 @@
  */ 
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Histogram{
+	// Change to true to sort histogram
+	public static boolean SORTED = false;
 
 	public static void main(String[] args) {
 		int[] test = {4, 2, 9, 1, 4, 12, 100, 100, 100, 100};
-		makeHistogram(test);
+		makeHistogram();
 	}
 
-	public static void makeHistogram(int[] nums){
+	public static void makeHistogram(){
+		// Get input for array length
+		int size = getSize();
+		// Ask for each value and build array
+		int[] nums = buildNumArray(size);
+		// Ask user if array is to be sorted
+		getSort();
 		// Count unique numbers
 		int unique_count = countUnique(makeString(nums));
-		// Initialize array
+		// Initialize 2d array
 		String[][] data = new String[unique_count][2];
-		// Populate array
+		// Populate 2d array
 		populateArray(data, makeString(nums));
+		// Sort 2d array
+		selectionSort(data);
 		// Print out histogram
-		printHistogram(data);
+		printHistogram(data);	
+	}
+
+	public static int getSize(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many integers do you wish to enter?: ");
+		int size = sc.nextInt();
+		// Validate input to make sure size is larger than 0
+		while (size <= 0){
+			System.out.println("Invalid input!");
+			System.out.print("How many integers do you wish to enter?: ");
+			size = sc.nextInt();
+		}
+		return size;
+	}
+
+	public static int[] buildNumArray(int size){
+		Scanner sc = new Scanner(System.in);
+		// Initialize array
+		int[] user_array = new int[size];
+		// Ask user for each number
+		// and add it to the array
+		for (int i = 0; i < size; i++) {
+			System.out.print("Enter value #" + (i + 1) + ": ");
+			int n = sc.nextInt();
+			user_array[i] = n;
+		}
+		return user_array;
+	}
+
+	public static void getSort(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Would you like your histogram to be sorted? (Y/N): ");
+		String response = sc.nextLine();
+		// Validate input
+		while (!response.toUpperCase().equals("Y") && !response.toUpperCase().equals("N")){
+			System.out.println("Invalid input!");
+			System.out.print("Would you like your histogram to be sorted? (Y/N): ");
+			response = sc.nextLine();
+		}
+		// Set SORTED value accordingly
+		if (response.toUpperCase().equals("Y")){
+			SORTED = true;
+		} else {
+			SORTED = false;
+		}
+		// New line for aesthetic purposes
+		System.out.println();
 	}
 
 	// Takes a integer array and returns a string array
@@ -34,6 +92,7 @@ public class Histogram{
 	}
 
 	// Iterate through each item, if item isn't a !, increment count and iterate setting all items equal to !
+	// Slightly spaghetti code, REFACTOR BEFORE UPLOADING
 	public static int countUnique(String[] nums_str){
 		int count = 0;
 		// Iterate through array
@@ -54,6 +113,7 @@ public class Histogram{
 	}
 
 	// Populate the new array
+	// Slightly spaghetti code, REFACTOR BEFORE UPLOADING
 	public static void populateArray(String[][] new_array, String[] nums_str){
 		int count = 0;
 		// Iterate through original array
@@ -73,7 +133,8 @@ public class Histogram{
 				count++;
 			}
 		}
-		System.out.println(Arrays.deepToString(new_array));
+		// Uncomment to print out 2d array for debugging
+		//System.out.println(Arrays.deepToString(new_array));
 	}
 
 	// Print out the histogram
@@ -114,5 +175,43 @@ public class Histogram{
 		for (int i = 0; i < spaces; i++) {
 			System.out.print(" ");
 		}
+	}
+
+	// Sort the 2d data array using selection sort
+	public static void selectionSort(String[][] data_array){
+		if (SORTED) {
+			// Iterate through each data point
+			for (int i = 0; i < data_array.length - 1; i++) {
+				// Find minimum value
+				int min_index = findMin(data_array, i);
+				// Swap with minimum value
+				// NOTE: Current value might be minimum, in which
+				// case it swaps with itself
+				swap(data_array, i, min_index);
+			}
+		}
+	} 
+
+	// Swaps two items in an array
+	public static void swap(String[][] list, int index1, int index2){
+		// Swap number 
+		String temp = list[index1][0];
+		list[index1][0] = list[index2][0];
+		list[index2][0] = temp;
+		// Swap amount 
+		temp = list[index1][1];
+		list[index1][1] = list[index2][1];
+		list[index2][1] = temp;
+	}
+
+	// Finds the minimum value in the data array after given index
+	public static int findMin(String[][] list, int index){
+		int min = index;
+		for (int i = index + 1; i < list.length ; i++) {
+			if (Integer.parseInt(list[i][0]) < Integer.parseInt(list[min][0])){
+				min = i;
+			}
+		}
+		return min;
 	}
 }
