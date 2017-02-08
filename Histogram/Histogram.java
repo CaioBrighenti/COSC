aa0a7c8e2d621a6bb9782decd6059fdcb528bd51
Caio Brighenti <Caio Brighenti>
2017-02-07 20:46:42 -0500

    Began countUnique function in Histogram

--- Histogram/Histogram.java
+++ Histogram/Histogram.java
@@ -13,6 +13,27 @@ public class Histogram{
 	public static void makeHistogram(int[] nums){
 		// Count unique numbers
 		// Initialize array
-		// 
+		// Populate array
+	}
+
+	public static int countUnique(int[] nums_str){
+		// Iterate through each item, if item isn't a !, increment count and iterate setting all items equal to !
+		int count = 0;
+		for (int i : nums_str) {
+			if (nums_str[i] != "!"){
+				count++;
+				for (int n : nums_str) {
+					// Problem here, store original value in variable to compare
+					if (nums_str[n] == nums[i]){
+						nums_str[n] = "!";
+					}
+				}
+			}
+		}
+		return count;
+	}
+
+	public static void populateArray(int[] new_array, int[] nums){
+
 	}
 }
