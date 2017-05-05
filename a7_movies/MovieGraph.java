03bd66643fb0acacc4311cc7a190f30a5b751b55
Caio Brighenti <Caio Brighenti>
2017-05-05 14:35:40 -0400

    Finished Movie assignment

--- a7_movies/MovieGraph.java
+++ a7_movies/MovieGraph.java
@@ -17,12 +17,12 @@ import java.util.Iterator;
 public class MovieGraph
 {
 	// Holds movies as keys, actors as values
-	private HashMap<String, LinkedList<String>> movieMap;
+	private HashMap<String, HashSet<String>> movieMap;
 	// Holds actors as keys, movies as values
-	private HashMap<String, LinkedList<String>> actorMap;
+	private HashMap<String, HashSet<String>> actorMap;
 
 	// Debugging output
-	private static final boolean DEBUG = true;
+	private static final boolean DEBUG = false;
 	private void pdbg(String s)
 	{
 		if (DEBUG)
@@ -33,8 +33,8 @@ public class MovieGraph
 	public MovieGraph(String filename) throws IOException, SecurityException, FileNotFoundException
 	{
 		// Initialize hash maps
-		movieMap = new HashMap<String, LinkedList<String>>();
-		actorMap = new HashMap<String, LinkedList<String>>();
+		movieMap = new HashMap<String, HashSet<String>>();
+		actorMap = new HashMap<String, HashSet<String>>();
 
 		// Initialize file and loop through each line
 		Scanner reader = new Scanner(new File(filename));		
@@ -47,11 +47,11 @@ public class MovieGraph
 
 			// Grab actors
 			String actors = line.substring(line.indexOf("/")+1);
-			LinkedList<String> actorList = new LinkedList<String>();
+			HashSet<String> actorSet = new HashSet<String>();
 			while(actors.indexOf("/") != -1){
 				int idx = actors.indexOf("/");
 				String actorName = actors.substring(0, idx);
-				actorList.add(actorName);
+				actorSet.add(actorName);
 				actors = actors.substring(idx+1);
 
 				// Update actor hash map
@@ -59,11 +59,11 @@ public class MovieGraph
 			}
 			// Ensure last actor is also caught
 			// This isn't very clear, need to figure out a better implementation of this
-			actorList.add(actors);
+			actorSet.add(actors);
 			addMovieToActor(actors, movieName);
 
 			// Add movie,actor combination to movie map
-			movieMap.put(movieName, actorList);
+			movieMap.put(movieName, actorSet);
 		}
 		System.out.println(actorMap);
 		System.out.println(movieMap);
@@ -78,9 +78,9 @@ public class MovieGraph
 		if (actorMap.containsKey(actor))
 			actorMap.get(actor).add(movie);
 		else{
-			LinkedList<String> movieList = new LinkedList<String>();
-			movieList.add(movie);
-			actorMap.put(actor, movieList);
+			HashSet<String> movieSet = new HashSet<String>();
+			movieSet.add(movie);
+			actorMap.put(actor, movieSet);
 		}
 	}
 
@@ -109,17 +109,17 @@ public class MovieGraph
 	// return the co-stars of an actor (over all movies)
 	// Pretty inneficient
 	// Grabs all movies actors is in, for each loops through all actors
-	// only adds actor to return list if actor is not already in list
+	// only adds actor to return Set if actor is not already in Set
 	public Iterable<String> getCoStars(String actor)
 	{
 		// for debugging
 		pdbg(actor);
 
-		LinkedList<String> movies = actorMap.get(actor);
-		LinkedList<String> actorsReturn = new LinkedList<String>();
+		HashSet<String> movies = actorMap.get(actor);
+		HashSet<String> actorsReturn = new HashSet<String>();
 		Iterator movieIterator = movies.iterator();
         while (movieIterator.hasNext()) {
-         	  LinkedList<String> actors = movieMap.get(movieIterator.next());
+         	  HashSet<String> actors = movieMap.get(movieIterator.next());
          	  Iterator actorIterator = actors.iterator();
          	  while (actorIterator.hasNext()){
          	  	String tempActor = (String)actorIterator.next();
@@ -152,9 +152,9 @@ public class MovieGraph
 		if (!actorMap.containsKey(actor1) || !actorMap.containsKey(actor2))
 			return null;
 
-		LinkedList<String> moviesReturn = new LinkedList<String>();
-		LinkedList<String> movies1 = actorMap.get(actor1);
-		LinkedList<String> movies2 = actorMap.get(actor2);
+		HashSet<String> moviesReturn = new HashSet<String>();
+		HashSet<String> movies1 = actorMap.get(actor1);
+		HashSet<String> movies2 = actorMap.get(actor2);
 		Iterator moviesIterator = movies1.iterator();
         while (moviesIterator.hasNext()){
          	String tempMovie = (String)moviesIterator.next();
