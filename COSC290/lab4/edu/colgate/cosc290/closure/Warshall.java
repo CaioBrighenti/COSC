1e0620406641ddadb99c22132f4e82fe88b72576
unknown <caio.brig@gmail.com>
2018-04-04 13:10:23 -0400

    Added lab 4 files

---
+++ COSC290/lab4/edu/colgate/cosc290/closure/Warshall.java
@@ -0,0 +1,21 @@
+// Colgate University COSC 290 Labs
+// Version 0.1,  2017
+// Author: Michael Hay
+package edu.colgate.cosc290.closure;
+
+import java.util.NoSuchElementException;
+
+/**
+ * A class for computing transitive closure using Warshall's algorithm
+ */
+public class Warshall {
+
+    /**
+     * Returns transitive closure of R.  This method uses Warshall's algorithm.
+     * @param R relation, represented as a matrix (double array)
+     * @return the transitive closure of R
+     */
+    public static boolean[][] transitiveClosure(boolean[][] R) {
+        throw new UnsupportedOperationException("implement me!");
+    }
+}
