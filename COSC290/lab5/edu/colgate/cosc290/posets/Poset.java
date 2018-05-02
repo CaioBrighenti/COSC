35f5d9d6a2657b516e8bf6767faa3a5a665afbbf
unknown <caio.brig@gmail.com>
2018-05-02 16:10:16 -0400

    Reworked maximal, implemented lower and upper bound functions

--- COSC290/lab5/edu/colgate/cosc290/posets/Poset.java
+++ COSC290/lab5/edu/colgate/cosc290/posets/Poset.java
@@ -42,7 +42,7 @@ public class Poset {
         // add this node to new list if not yet present
         if (returnH.get(ancestor) == null)
           returnH.put(ancestor, new LinkedList<>());
-        // iterate through each succesor for that node
+        // iterate through each successor for that node
         for (Integer succ: successors) {
           // add this node to new list if not yet present
           if (returnH.get(succ) == null)
@@ -58,13 +58,32 @@ public class Poset {
      * @return the set of maximal elements
      */
     public Set<Integer> maximal() {
+      return maximal(hasse.keySet());
+    }
+
+    /**
+     * Returns the set of maximal elements.
+     * @param S subset S⊆ P
+     * @return the set of maximal elements
+     */
+    public Set<Integer> maximal(Set<Integer> S) {
       Set<Integer> returnSet = new HashSet<>();
-      // iterate through the Hasse diagram
-      for (Map.Entry<Integer,List<Integer>> entry : hasse.entrySet()) {
-        Integer ancestor = entry.getKey();
-        List<Integer> successors = entry.getValue();
+      // iterate through each item in S and check if it has any successors
+      for (Integer ancestor : S) {
+        List<Integer> successors = hasse.get(ancestor);
+        // if no successors, must be maximal
         if (successors.isEmpty())
           returnSet.add(ancestor);
+        else {
+          // check if successors are in subset S⊆ P
+          boolean flag = true;
+          for (Integer successor : successors) {
+            if (S.contains(successor))
+              flag = false;
+          }
+          if (flag)
+            returnSet.add(ancestor);
+        }
       }
       return returnSet;
     }
@@ -74,11 +93,20 @@ public class Poset {
      * @return the maximum element or -1 if no such element exists
      */
     public int maximum() {
-      Set<Integer> maximalSet = maximal();
-      if (maximalSet.size() == 1)
-        return maximalSet.iterator().next();
-      else
-        return -1;
+      return maximum(hasse.keySet());
+    }
+
+    /**
+     * Returns the maximum element of a subset S⊆ P or -1 if no such element exists.
+     * @param S subset S⊆ P
+     * @return the maximum element of a subset S⊆ P or -1 if no such element exists.
+     */
+    public int maximum(Set<Integer> S) {
+      Set<Integer> maximalSet = maximal(S);
+        if (maximalSet.size() == 1)
+          return maximalSet.iterator().next();
+        else
+          return -1;
     }
 
     /**
@@ -97,11 +125,11 @@ public class Poset {
     public Set<Integer> minimal(Set<Integer> S) {
       Set<Integer> returnSet = new HashSet<>();
       // iterate through each item in S and check if it has any ancestors
-      for (Integer succesor : S) {
-        List<Integer> ancestors = invHasse.get(succesor);
+      for (Integer successors : S) {
+        List<Integer> ancestors = invHasse.get(successors);
         // if no ancestors, must be minimal
         if (ancestors.isEmpty())
-          returnSet.add(succesor);
+          returnSet.add(successors);
         else {
           // check if ancestors are in subset S⊆ P
           boolean flag = true;
@@ -110,7 +138,7 @@ public class Poset {
               flag = false;
           }
           if (flag)
-            returnSet.add(succesor);
+            returnSet.add(successors);
         }
       }
       return returnSet;
@@ -162,7 +190,26 @@ public class Poset {
      */
     public List<Integer> topoSort() {
         List<Integer> topoList = new ArrayList<Integer>();
-        Set<Integer> tempSet = hasse.keySet();
+        Set<Integer> tempSet = new HashSet<Integer>();
+        tempSet.addAll(hasse.keySet());
+        // iteratively add a minimal element to the topo order, then re-obtain the minimal elements
+        while (!tempSet.isEmpty()){
+          Set<Integer> minimalSet = minimal(tempSet);
+          topoList.addAll(minimalSet);
+          tempSet.removeAll(minimalSet);
+        }
+        return topoList;
+    }
+
+    /**
+     * Returns a topological sort of this posets using a DFS algorithm
+     * @return a list containing all of the elements in the set in ascending order (if x ≤ y according to the partial
+     * order, then the index of x should be less than the index of y in the returned list)
+     */
+    public List<Integer> topoSortDFS() {
+        List<Integer> topoList = new ArrayList<Integer>();
+        Set<Integer> tempSet = new HashSet<Integer>();
+        tempSet.addAll(hasse.keySet());
         // iteratively add a minimal element to the topo order, then re-obtain the minimal elements
         while (!tempSet.isEmpty()){
           Set<Integer> minimalSet = minimal(tempSet);
@@ -178,7 +225,27 @@ public class Poset {
      * @return a set of elements that correspond to a lower bound for S
      */
     public Set<Integer> lowerBound(Set<Integer> S) {
-        throw new UnsupportedOperationException("implement me!");
+      Set<Integer> returnSet = new HashSet<Integer>();
+      Set<Integer> minimals = minimal(S);
+      // if minimum present, must be upper bound (could call minimum() but this is more efficient)
+      if (minimals.size() == 1)
+        returnSet.addAll(minimals);
+      // search for common ancestors
+      Map<Integer, Integer> ancMap = new HashMap<>();
+      for (int minimal : minimals) {
+        Set<Integer> ancestors = getAncestors(minimal);
+        // keep track of how many minimals each element is a ancestor to
+        // if an element is ancestor to all minimals, add to reutrn set
+        for (int ancestor : ancestors) {
+          if (ancMap.containsKey(ancestor))
+            ancMap.put(ancestor, ancMap.get(ancestor)+1);
+          else
+            ancMap.put(ancestor, 1);
+          if (ancMap.get(ancestor) == minimals.size())
+            returnSet.add(ancestor);
+        }
+      }
+      return returnSet;
     }
 
     /**
@@ -187,7 +254,65 @@ public class Poset {
      * @return a set of elements that correspond to a upper bound for S
      */
     public Set<Integer> upperBound(Set<Integer> S) {
-        throw new UnsupportedOperationException("implement me!");
+      Set<Integer> returnSet = new HashSet<Integer>();
+      Set<Integer> maximals = maximal(S);
+      // if maximum present, must be upper bound (could call maximum() but this is more efficient)
+      if (maximals.size() == 1)
+        returnSet.addAll(maximals);
+      // search for common successors
+      Map<Integer, Integer> succMap = new HashMap<>();
+      for (int maximal : maximals) {
+        Set<Integer> successors = getSuccessors(maximal);
+        // keep track of how many maximals each element is a successor to
+        // if an element is successor to all maximals, add to reutrn set
+        for (int successor : successors) {
+          if (succMap.containsKey(successor))
+            succMap.put(successor, succMap.get(successor)+1);
+          else
+            succMap.put(successor, 1);
+          if (succMap.get(successor) == maximals.size())
+            returnSet.add(successor);
+        }
+      }
+      return returnSet;
+    }
+
+    /**
+    * Returns all successors of element a∈ P using DFS
+    * @param a a element a∈ P
+    * @return a set of all elements b in P where a⪯b, or empty set if none
+    */
+    public Set<Integer> getSuccessors(Integer a){
+      Set<Integer> returnSet = new HashSet<>();
+      if (hasse.get(a).isEmpty())
+        return returnSet;
+      else {
+        // make recursive call, add all successors of successor
+        for (int succ : hasse.get(a)) {
+          returnSet.add(succ);
+          returnSet.addAll(getSuccessors(succ));
+        }
+      }
+      return returnSet;
+    }
+
+    /**
+    * Returns all ancestors of element a∈ P using DFS
+    * @param a a element a∈ P
+    * @return a set of all elements b in P where b⪯a, or empty set if none
+    */
+    public Set<Integer> getAncestors(Integer a){
+      Set<Integer> returnSet = new HashSet<>();
+      if (invHasse.get(a).isEmpty())
+        return returnSet;
+      else {
+        // make recursive call, add all ancestors of ancestor
+        for (int anc : invHasse.get(a)) {
+          returnSet.add(anc);
+          returnSet.addAll(getAncestors(anc));
+        }
+      }
+      return returnSet;
     }
 
     /**
@@ -196,7 +321,7 @@ public class Poset {
      * @return the GLB or -1 if it does not exists
      */
     public int GLB(Set<Integer> S) {
-        throw new UnsupportedOperationException("implement me!");
+        return maximum(lowerBound(S));
     }
 
     /**
@@ -205,7 +330,7 @@ public class Poset {
      * @return the LUB or -1 if it does not exists
      */
     public int LUB(Set<Integer> S) {
-        throw new UnsupportedOperationException("implement me!");
+        return minimum(upperBound(S));
     }
 
     public static void main(String[] args) {
@@ -239,6 +364,14 @@ public class Poset {
         List<Integer> totalOrd = new ArrayList<>(Arrays.asList(arr2));
         System.out.println("poset.consistent({ 0,1,2,3,4 }): " + poset.consistent(totalOrd));
         System.out.println("poset.topoSort(): " + poset.topoSort());
+        Integer arr3[] = { 0,1,2 };
+        Set<Integer> subset2 = new HashSet<>(Arrays.asList(arr3));
+        System.out.println("poset.upperBound({0,1,2}): " + poset.upperBound(subset2));
+        System.out.println("poset.LUB({0,1,2}): " + poset.LUB(subset2));
+        Integer arr4[] = { 2,4 };
+        Set<Integer> subset3 = new HashSet<>(Arrays.asList(arr4));
+        System.out.println("poset.lowerBound({2,4}): " + poset.lowerBound(subset3));
+        System.out.println("poset.GLB({2,4}): " + poset.GLB(subset3));
     }
 
     /**
