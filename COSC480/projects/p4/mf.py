02ad59ac51abefee3ee5bdfde0295f28c7bee1a2
Caio Brighenti <caio.brig@gmail.com>
2019-12-08 21:10:47 -0500

    finish implementing 480 p4

--- COSC480/projects/p4/mf.py
+++ COSC480/projects/p4/mf.py
@@ -8,69 +8,85 @@ from tqdm import tqdm
 def error_function(r,u,v,mu,a,b):
     return r - np.dot(u,v) - mu - a - b
 
-def sgd(num_iter,train,eta,lambda_reg,U,V,mu,a,b):
-    num_ratings = np.shape(train)[0]
+def rmse(h, r):
+    resid = h - r
+    cost = np.sqrt(np.mean(resid**2))
+    return cost
+
+def predict(user, movie, U, V, mu, a, b):
+    '''
+    PREDICT Make predictions for user/movie pairs
+    Inputs: 
+      model parameters
+      user               vector of users
+      movie              vector of movies
+    
+    Output:
+      predictions        vector of predictions
+    '''    
+    user_weights = U[user,:]
+    movie_weights = V[movie,:]
+    user_biases = a[user]
+    movie_biases = b[movie]
+    predictions = np.sum(np.multiply(user_weights,movie_weights),axis=1)
+    predictions = predictions + mu + user_biases + movie_biases
+    return predictions
+
+def sgd(num_iter,train_user,train_movie,train_rating,eta,lambda_reg,U,V,mu,a,b,valid_user,valid_movie,valid_rating):
+    num_ratings = len(train_rating)
     rating_ids = np.arange(num_ratings)
+    train_rmse = np.zeros(num_iter)
+    valid_rmse = np.zeros(num_iter)
     for j in tqdm(range(num_iter)):
         np.random.shuffle(rating_ids)
         for id in rating_ids:
-            review = train.iloc[id,]
-            u_id = review.loc['user_id']
-            v_id = review.loc['movie_id']
-            r = review.loc['rating']
+            r = train_rating[id]
+            u_id = train_user[id]
+            v_id = train_movie[id]
+            u_weights = U[u_id]
+            v_weights = V[v_id]
             # compute e
-            e = error_function(r,U[u_id,:],V[v_id,:],mu,a[u_id],b[v_id])
-            # update u
-            u_update = np.multiply(
-                np.subtract(
-                    np.multiply(e,V[v_id,:]),
-                    np.multiply(lambda_reg,U[u_id,:])
-                ),
-                eta
-            )
-            U[u_id,:] = U[u_id,:] + u_update
-            # update v
-            v_update = np.multiply(
-                np.subtract(
-                    np.multiply(e,U[u_id,:]),
-                    np.multiply(lambda_reg,V[v_id,:])
-                ),
-                eta
-            )
-            V[v_id,:] = V[v_id,:] + v_update
+            e = error_function(r,u_weights,V[v_id],mu,a[u_id],b[v_id])
+            # compute updates
+            u_update = eta * ((e * v_weights) - (lambda_reg * u_weights))
+            v_update = eta * ((e * u_weights) - (lambda_reg * v_weights))
+            # perform updates
+            U[u_id] = u_weights + u_update
+            V[v_id] = v_weights + v_update
+            # update biases a,b
+            a_update = eta * (e - (lambda_reg * a[u_id]))
+            a[u_id] = a[u_id] + a_update
+            b_update = eta * (e - (lambda_reg * b[v_id]))
+            b[v_id] = b[v_id] + b_update
+        # make predictions to track error
+        train_rmse[j] = rmse(predict(train_user, train_movie, U, V, mu, a, b),train_rating)
+        valid_rmse[j] = rmse(predict(valid_user, valid_movie, U, V, mu, a, b),valid_rating)
+    return U,V,a,b,train_rmse,valid_rmse
+
+def sgd_minimal(num_iter,train_user,train_movie,train_rating,eta,lambda_reg,U,V,mu,a,b):
+    num_ratings = len(train_rating)
+    rating_ids = np.arange(num_ratings)
+    for j in range(num_iter):
+        np.random.shuffle(rating_ids)
+        for id in rating_ids:
+            r = train_rating[id]
+            u_id = train_user[id]
+            v_id = train_movie[id]
+            u_weights = U[u_id]
+            v_weights = V[v_id]
+            # compute e
+            e = error_function(r,u_weights,V[v_id],mu,a[u_id],b[v_id])
+            # compute updates
+            u_update = eta * ((e * v_weights) - (lambda_reg * u_weights))
+            v_update = eta * ((e * u_weights) - (lambda_reg * v_weights))
+            # perform updates
+            U[u_id] = u_weights + u_update
+            V[v_id] = v_weights + v_update
             # update biases a,b
             a_update = eta * (e - (lambda_reg * a[u_id]))
             a[u_id] = a[u_id] + a_update
             b_update = eta * (e - (lambda_reg * b[v_id]))
             b[v_id] = b[v_id] + b_update
     return U,V,a,b
-
-
-
-def sgd_vectorized(num_iter,train,eta,lambda_reg,U,V):
-    num_ratings = np.shape(train)[0]
-    for j in tqdm(range(1)):
-        u_ids = train.user_id
-        v_ids = train.movie_id
-        R = train.rating
-        # extract U,V
-        print(np.shape(U))
-        print(np.shape(V))
-        print(np.shape(u_ids))
-        print(np.shape(v_ids))
-        print(np.shape(R))
-        # compute e
-        # e = error_function(r,U[u_id,:],V[v_id,:])
-        # # update u
-        # u_update = np.multiply(
-        #     np.subtract(
-        #         np.multiply(e,V[v_id,:]),
-        #         np.multiply(lambda_reg,U[u_id,:])
-        #     ),
-        #     eta
-        # )
-        # U[u_id,:] = U[u_id,:] + u_update
-
-    return U,V
 
     
